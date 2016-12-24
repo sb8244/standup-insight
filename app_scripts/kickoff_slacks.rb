b121d5d37a4ac88cea47a20b44d0519082ad1128
Stephen Bussey <steve.bussey@salesloft.com>
2016-12-24 18:18:28 -0500

    don’t run this on the weekends

--- app_scripts/kickoff_slacks.rb
+++ app_scripts/kickoff_slacks.rb
@@ -1,3 +1,10 @@
+now = Time.now.in_time_zone("US/Eastern")
+
+if ENV["CHECK_INTERVAL"] && (now.wday == 6 || now.wday == 0) # don't run on weekends
+  p "exiting"
+  exit
+end
+
 threads = SlackUserMapping.find_each.map do |user_mapping|
   stand_up = user_mapping.group.todays_standup
   Thread.new { SlackSessionKickoffJob.perform_now(user_mapping, stand_up) }
