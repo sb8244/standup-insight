37e36ba64feda45259435393a01f858e50ddf218
Stephen Bussey <steve.bussey@salesloft.com>
2016-12-24 11:33:10 -0500

    allow either to be nil

--- app/models/slack_api.rb
+++ app/models/slack_api.rb
@@ -3,10 +3,11 @@ class SlackApi
     @web_client = Slack::Web::Client.new(token: token)
   end
 
-  def find_user_id(email:, username:)
+  def find_user_id(email: nil, username: nil)
     member = web_client.users_list.members.find do |member|
       next if member.is_bot
-      member.name == username || member.profile.email == email
+      (username.present? && member.name == username) ||
+      (email.present? && member.profile.email == email)
     end
 
     member.try!(:id)
