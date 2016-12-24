207d8b06a3751b248e6c5b4029ccfc197a2453b5
Stephen Bussey <steve.bussey@salesloft.com>
2016-12-24 11:32:28 -0500

    Need to read user’s email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Devise.setup do |config|
-  config.omniauth :slack, ENV.fetch('SLACK_API_KEY'), ENV.fetch('SLACK_API_SECRET'), scope: 'identify,bot'
+  config.omniauth :slack, ENV.fetch('SLACK_API_KEY'), ENV.fetch('SLACK_API_SECRET'), scope: 'identify,bot,users:read,users:read.email'
 end
